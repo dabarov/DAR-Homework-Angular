044ea3ed710c298dc2793b8832d3184145b02965
Aldiyar Dabarov <simplyalde@users.noreply.github.com>
2020-08-12 18:58:10 +0600

    Table, Profile, Back link

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
-import { User } from 'src/app/shared/types';
+import { SingleUser } from 'src/app/shared/types';
 
 @Component({
   selector: 'app-user',
@@ -9,14 +9,16 @@ import { User } from 'src/app/shared/types';
   styleUrls: ['./user.component.scss'],
 })
 export class UserComponent {
-  user: User;
+  user: SingleUser;
   constructor(private route: ActivatedRoute, private httpClient: HttpClient) {}
 
   ngOnInit() {
     this.route.params.subscribe((params) => {
       if (params && params.id) {
         this.httpClient
-          .get<User>(`https://jsonplaceholder.typicode.com/users/${params.id}`)
+          .get<SingleUser>(
+            `https://jsonplaceholder.typicode.com/users/${params.id}`
+          )
           .subscribe((user) => (this.user = user));
       }
     });
